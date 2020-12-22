f69fc3158a9e7d66e44ae03b95190d52cfe9d2a8
sumesh3060 <sumesh3060@gmail.com>
2020-12-22 18:28:35 +0530

    Run

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ require("dotenv").config();
 const cors = require('cors');
 app.use(cors());
 app.options('*', cors());
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 // models
 var models = require("./models");
 
